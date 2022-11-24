009e03857268f9730736521b46abc8899d101c7b
jefferson <jefferson.orejuela.sannicolas@utelvt.edu.ec>
2022-11-24 10:03:22 -0600

    calcula edad

--- orejuela_jefferson/calculaEdad.cpp
+++ orejuela_jefferson/calculaEdad.cpp
@@ -3,35 +3,35 @@
 //asignatura:Tics B
 //docente:Ing.Stalin Francis
 
-#include<iostream>//
-using namespace std;//
-int main()//
+#include<iostream>//permite la entrada y salida de datos en la libreria.
+using namespace std;//llamada espacio de nombre para utilizar cin, cout y no estar utlizando siempre el std.
+int main()//el cuerpo del programa que indica donde comienza y finaliza.
 
-{//
-	int aa,ma,da,an,mn,dn,a,m,d;
-	cout<<"ingrese la fecha actual: ";
-	cin>>aa>>ma>>da;
+{
+	int aa,ma,da,an,mn,dn,a,m,d;//declaramos los variables
+	cout<<"ingrese la fecha actual: ";//muestra mensaje al usuario en pantalla para que ingrese el dato.
+	cin>>aa>>ma>>da;//guarda los datos ingresado del usuario
 
-	cout<<"ingrese la fecha de nacimiento:  ";
-	cin>>an>>mn>>dn;
-	if(da>dn){
-		d=da-dn;
+	cout<<"ingrese la fecha de nacimiento:  ";//el usuario tiene que ingresar el dato que sale en la pantalla
+	cin>>an>>mn>>dn;//guardamos el dato ingresado por el usuario
+	if(da>dn){//utilizamos cuando es verdadero la condicion
+		d=da-dn;//declarmos variable de dia 
 
-	}else{
-		da=da+30;
-		ma=ma-1;
-		d=da-dn;
+	}else{//utilizamos cuando la condicion no se cumple,(si es falso)
+		da=da+30;//declaramos dia actual
+		ma=ma-1;//decalramos mes actual
+		d=da-dn;//declaramos dia 
 
 	}
-	if(ma>mn){
-		m=ma-mn;
-	}else{
-		ma=ma+12;
-		aa=aa-an;
-		m=ma-mn;
+	if(ma>mn){//utilizamos si es verdadero
+		m=ma-mn;//escribimos el mes 
+	}else{//utilizamos si la condicion es falso
+		ma=ma+12;//escribimos mes actual para finalizar
+		aa=aa-an;//escribimos año actual para finalizar
+		m=ma-mn;//escribimos el mes general
 	}
-	a=aa-an;
-	cout<<"usted tiene:  "<<a<<"años,"<<d<<"meses,"<<d<<"dias,"<<endl;
-	return 0;
+	a=aa-an;//escribimos el año general para fianlizar
+	cout<<"usted tiene:  "<<a<<"años,"<<d<<"meses,"<<d<<"dias,"<<endl;//mostramos al usuario el mensaje en pantalla el resultado
+	return 0;//finaliza la ejecucion y devuelve el control a la funcion de llamada.
 
 }
